3a1e5e2948062d4ec56ae9f83799ca69d2e1180a
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-21 02:36:35 -0500

    Add REST api

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
-    "fmt"
+    //"encoding/json"
+    //"fmt"
+    //"log"
+    "strconv"
     "math/big"
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+
 )
 
 func factorial(n int64) *big.Int {
@@ -12,7 +19,25 @@ func factorial(n int64) *big.Int {
 }
 
 func main() {
-    var n int64
-    n = 10
-    fmt.Println(factorial(n))
+    router := gin.Default()
+
+	// This handler will match /user/john but will not match /user/ or /user
+	router.GET("/factorial/:n", func(c *gin.Context) {
+		n := c.Param("n")
+        param, err := strconv.ParseInt(n, 10, 64)
+        if err != nil {
+            panic(err)
+            c.String(http.StatusBadRequest, "Must provide a number")
+        }
+
+		c.String(http.StatusOK, factorial(param).String())
+	})
+
+	router.Run(":8080")
 }
+
+//func main() {
+    //var n int64
+    //n = 10000
+    //fmt.Println(factorial(n))
+//}

365f6ab023e433f8bbc8cc7cf6e1c851ca80e46c
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-21 08:39:03 -0500

    Run go fmt

--- main.go
+++ main.go
@@ -1,34 +1,33 @@
 package main
 
 import (
-    //"encoding/json"
-    //"fmt"
-    //"log"
-    "strconv"
-    "math/big"
-    "net/http"
+	//"encoding/json"
+	//"fmt"
+	//"log"
+	"math/big"
+	"net/http"
+	"strconv"
 
-    "github.com/gin-gonic/gin"
-
+	"github.com/gin-gonic/gin"
 )
 
 func factorial(n int64) *big.Int {
-    x := new(big.Int)
-    x.MulRange(1, n)
-    return x
+	x := new(big.Int)
+	x.MulRange(1, n)
+	return x
 }
 
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 
 	router.GET("/factorial/:n", func(c *gin.Context) {
 		n := c.Param("n")
-        param, err := strconv.ParseInt(n, 10, 64)
-        // Handle non numbers strings and negatives
-        if err != nil || param < 0 {
-            c.String(http.StatusBadRequest, "Must provide a number greater or equal than 0")
-            return
-        }
+		param, err := strconv.ParseInt(n, 10, 64)
+		// Handle non numbers strings and negatives
+		if err != nil || param < 0 {
+			c.String(http.StatusBadRequest, "Must provide a number greater or equal than 0")
+			return
+		}
 
 		c.String(http.StatusOK, factorial(param).String())
 	})

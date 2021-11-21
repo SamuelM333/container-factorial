9e736135244d2dddd2fd928968ce0dfdb6f8fce4
Samuel Murillo <samuelmurillo333@gmail.com>
2021-11-21 08:37:42 -0500

    Handle non numbers and negatives

--- main.go
+++ main.go
@@ -21,13 +21,13 @@ func factorial(n int64) *big.Int {
 func main() {
     router := gin.Default()
 
-	// This handler will match /user/john but will not match /user/ or /user
 	router.GET("/factorial/:n", func(c *gin.Context) {
 		n := c.Param("n")
         param, err := strconv.ParseInt(n, 10, 64)
-        if err != nil {
-            panic(err)
-            c.String(http.StatusBadRequest, "Must provide a number")
+        // Handle non numbers strings and negatives
+        if err != nil || param < 0 {
+            c.String(http.StatusBadRequest, "Must provide a number greater or equal than 0")
+            return
         }
 
 		c.String(http.StatusOK, factorial(param).String())
@@ -35,9 +35,3 @@ func main() {
 
 	router.Run(":8080")
 }
-
-//func main() {
-    //var n int64
-    //n = 10000
-    //fmt.Println(factorial(n))
-//}
